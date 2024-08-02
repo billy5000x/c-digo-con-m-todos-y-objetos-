a1c47d0a4365341c36341f2d69787450cfedace8
Billy5000x <jhoansnf02@gmail.com>
2024-08-02 15:56:18 -0500

    se declara la clase en la rama principal main

---
+++ metodos y objetos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CajeroAutomatico cajero = new CajeroAutomatico();
+        cajero.Iniciar();
+    }
+}
+
